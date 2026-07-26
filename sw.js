a056e96504fbec74760cbb2bf3cc6f35fcde5717
obedkabakaba <obedkabakaba01@gmail.com>
2026-07-26 01:31:08 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
    ils continueraient d'utiliser l'ancienne version des pages.
    ========================================================================== */
 
-const VERSION = 'ardoise-v19';
+const VERSION = 'ardoise-v20';
 const CACHE_COQUILLE = `${VERSION}-coquille`;
 const CACHE_DONNEES = `${VERSION}-donnees`;
 
